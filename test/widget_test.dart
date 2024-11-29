817d99fb76a182888c1ec071142b046bd0578a2a
delepover56 <taha82426980@gmail.com>
2024-11-29 06:19:48 +0500

    2nd Update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:baby_shop/main.dart';
+import 'package:baby_shop_hub/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
